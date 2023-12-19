8f2535069cad5577ed506b3d13f518709d59bd78
farhanawdpf <farhanawdpf@gmail.com>
2023-12-19 12:36:20 +0600

    update-final

--- 2/src/components/TheWelcome.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>helloo</div>
-</template>
--- vue-laravel/2/src/views/HomeView.vue
+++ vue-laravel/2/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-     <div> 
+     <div class="container"> 
       <div class="card" style="width: 95%; padding:5%">
         <div class="card-header"> 
          <h4> Employee
@@ -13,58 +13,36 @@
           <table class="table table-success table-striped"> 
             <thead> 
               <tr> 
-                <th>SL</th>
-                <th>Name</th>
-                <th>Contact</th>
-                <th>Email</th>
-                <th>Action</th>
+                <th scope="col">ID</th>
+                <th scope="col">Employee Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">Mobile</th>
+                <th scope="col">Option</th>
               </tr>
             </thead>
             <tbody> 
-              <tr> 
-                <td>01</td>
-                <td>Mila</td>
-                <td>1231231</td>
-                <td>er@gmail.com</td>
+
+              <tr v-for="employee in result" v-bind:key="employee.id">
+          
+                <td>{{ employee.id }}</td>
+                <td>{{ employee.name }}</td>
+                <td>{{ employee.address }}</td>
+                <td>{{ employee.mobile }}</td>
                 <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                  <button>view</button>
+                  <button type="button" class="btn btn-warning" @click="edit(employee)">Edit</button>
+                  <button type="button" class="btn btn-danger"  @click="remove(employee)">Delete</button>
                 </td>
               </tr>
-              <tr> 
-                <td>01</td>
-                <td>Mila</td>
-                <td>1231231</td>
-                <td>er@gmail.com</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                  <button>view</button>
-                </td>
-              </tr>
-              <tr> 
-                <td>02</td>
-                <td>Mila</td>
-                <td>1223431231</td>
-                <td>pr@gmail.com</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                  <button>view</button>
-                </td>
-              </tr>
-              <tr> 
-                <td>03</td>
-                <td>Shila</td>
-                <td>1231231</td>
-                <td>er@gmail.com</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                  <button>view</button>
-                </td>
-              </tr>
+              <!-- <tr v-for="(student,index) in this.students" :key="index"> 
+                <td>{{ student.id }}</td>
+               <td> 
+                <router-link to="/" class="btn btn-sucess">
+                  edit
+                </router-link>
+                <button type="button" class="btn btn-danger">Delete</button>
+               </td>
+                
+              </tr> -->
             </tbody>
           </table>
         </div>
@@ -74,12 +52,41 @@
   </main>
 </template>
 <script> 
+import axios from 'axios'
  export default { 
   name: 'home',
   data() {
     return {
-      home: '',
-    };
+      result: []
+    }
+  },
+  // created() { 
+  //       this.getEmployees();
+  //   },
+  mounted(){ 
+    this.getEmployees();
+    console.log("mounted() called.......");
+  },
+
+  methods: {
+    // getEmployees(){ 
+    //   axios.get('http://127.0.0.1:8000/api/employees').then(res => { 
+    //     this.employee = res.data.employees
+    //     // console.log(res)
+    //   });
+    // }
+
+    getEmployees()
+           {
+                 var page = "http://127.0.0.1:8000/api/employees";
+                 axios.get(page)
+                  .then(
+                      ({data})=>{
+                        console.log(data);
+                        this.result = data;
+                      }
+                  );
+           }
   },
  }
 </script>
